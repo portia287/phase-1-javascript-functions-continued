db20f6c4aa723534de004c1fe36239f4c4531ad3
Portia Antwi <portiaantwi257@gmail.com>
2022-06-13 11:00:19 -0700

    commit tree 7c8c3c941450a47efe224f10d7b62aab8b2ac704

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function saturdayFun(message = "This Saturday, I want to roller-skate!"){
+    console.log (message);
+    if(message === 'This Saturday, I want to roller-skate!') {
+        return message 
+    }
+    
+
+ else
+      return "This Saturday, I want to bathe my dog!"
+
+      }
+
+function mondayWork(message ="This Monday, I will go to the office." ){
+console.log(message)
+if(message === "This Monday, I will go to the office."){
+  return message
+} else
+return "This Monday, I will work from home."
+
+
+}
+function wrapAdjective(result= '*') {
+let emphatic = `You are`
+return function (inner = "handsome")
+
+}
